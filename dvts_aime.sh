9a18def7e2a161019658b18758b9e96ab013a623
Hossam Amer <hossam.amer12@gmail.com>
2025-03-16 08:34:05 -0400

    update the index of aime

--- dvts_aime.sh
+++ dvts_aime.sh
@@ -2,11 +2,12 @@
 
 # time python scripts/test_time_compute.py recipes/TinyLlama_v1.1_math_code/dvts.yaml --seed=1 --search_batch_size=25 --prm_batch_size=1 --dataset_start=0 --dataset_end=10 --n=64 --model_path="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-100" --prm_path="Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B"
 
-MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-100"
+# MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-100"
 # MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-200"
+MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-300"
 
 set -evx
-for ((i=0; i<=90; i+=10)); do
+for ((i=0; i<90; i+=10)); do
 # i=0
     time python scripts/test_time_compute.py recipes/TinyLlama_v1.1_math_code/dvts.yaml \
         --seed=1 --search_batch_size=25 --prm_batch_size=1 \
@@ -15,3 +16,7 @@ for ((i=0; i<=90; i+=10)); do
         --prm_path="Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B" --gpu_memory_utilization=0.45 --beam_width=2
 done
 
+# checkpoint 200
+# real    88m19.344s
+# user    133m47.798s
+# sys     12m5.350s
