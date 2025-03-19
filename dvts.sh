08790ae073380af11a1709868bda05695a30a9a2
Hossam Amer <hossam.amer12@gmail.com>
2025-03-19 14:42:55 -0400

    getting dvts back to the original state

--- dvts.sh
+++ dvts.sh
@@ -7,8 +7,8 @@ MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-100"
 # MODEL="/home/m00918254/TTC-checkpoints/tinyllama-math-code-checkpoint-200"
 # MODEL="/home/hossamamer/TTC_checkpoints/TTC-checkpoints/tinyllama-math-code-checkpoint-300"
 # MODEL="/home/hossamamer/TTC_checkpoints/TTC-checkpoints/tinyllama-math-code-checkpoint-200"
-# RECIPE=recipes/TinyLlama_v1.1_math_code/dvts.yaml
-RECIPE=recipes/TinyLlama_v1.1_math_code/best_of_n.yaml
+RECIPE=recipes/TinyLlama_v1.1_math_code/dvts.yaml
+# RECIPE=recipes/TinyLlama_v1.1_math_code/best_of_n.yaml
 
 
 for ((i=0; i<500; i+=10)); do
