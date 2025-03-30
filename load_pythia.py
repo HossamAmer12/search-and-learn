82ca517945c7c531a50f560ad252fb6f8dfe67e3
Hossam Amer <hossam.amer12@gmail.com>
2025-03-30 13:07:36 -0400

    update running notes with pythia training notes

--- load_pythia.py
+++ load_pythia.py
@@ -1,13 +1,23 @@
-# EleutherAI/pythia-70m-deduped
+# EleutherAI/pythia-410m-deduped
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-1b-deduped",
-revision="step143000",
-cache_dir="./pythia-1b-deduped/step143000",
+# sampled_ckpt_list = [1000, 20000, 40000, 80000, 100000, 120000, 143000]
+
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-410m-deduped",
+revision="step80000",
+cache_dir="/dataset/pythia_models//pythia-410m-deduped/step80000",
 )
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-1b-deduped",
-revision="step143000",
-cache_dir="./pythia-1b-deduped/step143000",
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-410m-deduped",
+revision="step80000",
+cache_dir="/dataset/pythia_models/pythia-410m-deduped/step80000",
 )
 
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-410m-deduped",
+revision="step143000",
+cache_dir="/dataset/pythia_models//pythia-410m-deduped/step143000",
+)
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-410m-deduped",
+revision="step143000",
+cache_dir="/dataset/pythia_models/pythia-410m-deduped/step143000",
+)
