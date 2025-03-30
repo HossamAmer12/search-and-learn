1594a9fd22ddd96d84e6298147bde07fa509b1e4
Hossam Amer <hossam.amer12@gmail.com>
2025-03-30 13:29:16 -0400

    add more running notes for pythia

--- loop_baseline.sh
+++ loop_baseline.sh
@@ -31,9 +31,22 @@ MODEL="/home/hossamamer/TTC_workspace/evaluate_math_baseline/pythia-70m-deduped/
 MODEL="/dataset/pythia_models/saved_models/pythia-sft-prm800/70m/from-checkpoint-143000/checkpoint-144"
 
 
-MODEL="/dataset/pythia_models/pythia-410m-deduped/step143000/models--EleutherAI--pythia-410m-deduped/snapshots/c0b6bef7dd1ec11d3baa07ee955de98a414dd464/"
-# MODEL=" /dataset/pythia_models/saved_models/pythia-sft-prm800/410m/from-checkpoint-143000/"
-# MODEL=" /dataset/pythia_models/saved_models/pythia-sft-prm800/410m/from-checkpoint-80000/"
+MODEL_PATHS=(
+    "/dataset/pythia_models/pythia-410m-deduped/step143000/models--EleutherAI--pythia-410m-deduped/snapshots/c0b6bef7dd1ec11d3baa07ee955de98a414dd464/"
+    "/dataset/pythia_models/saved_models/pythia-sft-prm800/410m/from-checkpoint-143000/"
+    "/dataset/pythia_models/saved_models/pythia-sft-prm800/410m/from-checkpoint-80000/"
+)
+
+for MODEL in "${MODEL_PATHS[@]}"; do
+    echo "Processing model at: $MODEL"
+    RECIPE=recipes/TinyLlama_v1.1_math_code/dvts.yaml
+    i=0
+    time python scripts/test_time_compute.py $RECIPE \
+        --seed=1 --search_batch_size=100 --prm_batch_size=1 \
+        --dataset_start=$i --dataset_end=$((i+500)) \
+        --n=1 --model_path=$MODEL \
+        --prm_path="Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B" --gpu_memory_utilization=0.45 --beam_width=1
+done
 
 
 # MODEL="/home/m00918254/TTC-checkpoints/tinyllama-sft-prm800/from-checkpoint-31908"
@@ -51,10 +64,10 @@ RECIPE=recipes/TinyLlama_v1.1_math_code/dvts.yaml
 # done
 
 # for ((i=0; i<500; i+=10)); do
-i=0
-    time python scripts/test_time_compute.py $RECIPE \
-        --seed=1 --search_batch_size=100 --prm_batch_size=1 \
-        --dataset_start=$i --dataset_end=$((i+500)) \
-        --n=1 --model_path=$MODEL \
-        --prm_path="Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B" --gpu_memory_utilization=0.45 --beam_width=1
+# i=0
+#     time python scripts/test_time_compute.py $RECIPE \
+#         --seed=1 --search_batch_size=100 --prm_batch_size=1 \
+#         --dataset_start=$i --dataset_end=$((i+500)) \
+#         --n=1 --model_path=$MODEL \
+#         --prm_path="Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B" --gpu_memory_utilization=0.45 --beam_width=1
 # done
